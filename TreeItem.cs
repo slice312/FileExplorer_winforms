35b20f486046f605f07496831ae13d961c668373
Talgat <tairovtalgat@gmail.com>
2019-06-01 00:07:41 +0600

    add file tree

---
+++ TreeItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FileExplorer
+{
+    public class TreeItem
+    {
+        private TreeItem mParentItem;
+        private List<TreeItem> mChildItems;
+        public string ItemData { get; set; }
+
+        public static int Count { get; private set; } = 0;
+
+
+        public TreeItem(string data, TreeItem parent = null)
+        {
+            Count++;
+            this.mParentItem = parent;
+            this.ItemData = data;
+            mChildItems = new List<TreeItem>(); //TODO capacity
+//            Console.WriteLine("mChildItems.Capacity = {0}", mChildItems.Capacity);
+        }
+
+
+        public void AddChild(TreeItem child)
+        {
+            mChildItems.Add(child);
+        }
+
+    }
+}
